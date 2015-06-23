6820e5a8af63f4ed18191db44f07973533cb812f
Paul Williams <paul@skenmy.com>
2015-06-23 20:28:00 +0100

    Upgrade to Laravel 5.1

--- app/Http/Controllers/Auth/AuthController.php
+++ app/Http/Controllers/Auth/AuthController.php
@@ -1,12 +1,9 @@
 <?php namespace vAMSYS\Http\Controllers\Auth;
 
-use Illuminate\Contracts\Auth\Guard;
-use Illuminate\Contracts\Auth\Registrar;
 use Illuminate\Http\Request;
-use Illuminate\Contracts\Auth\User;
-use Illuminate\Support\Facades\Log;
+use vAMSYS\User;
+use Illuminate\Support\Facades\Validator;
 use Illuminate\Support\Facades\Session;
-use vAMSYS\Airline;
 use vAMSYS\Http\Controllers\Controller;
 use Illuminate\Foundation\Auth\AuthenticatesAndRegistersUsers;
 use vAMSYS\Pilot;
@@ -24,11 +21,8 @@ class AuthController extends Controller {
 	 */
 	protected $redirectTo;
 
-	public function __construct(Guard $auth, Registrar $registrar)
+	public function __construct()
 	{
-		$this->auth = $auth;
-		$this->registrar = $registrar;
-
 		$this->middleware('guest', ['except' => ['getLogout']]);
 	}
 
@@ -87,5 +81,37 @@ class AuthController extends Controller {
 				'login' => 'Invalid Credentials',
 			]);
 	}
+
+  /**
+   * Get a validator for an incoming registration request.
+   *
+   * @param  array  $data
+   * @return \Illuminate\Contracts\Validation\Validator
+   */
+  public function validator(array $data)
+  {
+    return Validator::make($data, [
+      'first_name' => 'required|max:255',
+      'last_name' => 'required|max:255',
+      'email' => 'required|email|max:255|unique:users',
+      'password' => 'required',
+    ]);
+  }
+
+  /**
+   * Create a new user instance after a valid registration.
+   *
+   * @param  array  $data
+   * @return User
+   */
+  public function create(array $data)
+  {
+    return User::create([
+      'first_name' => $data['first_name'],
+      'last_name' => $data['last_name'],
+      'email' => $data['email'],
+      'password' => bcrypt($data['password']),
+    ]);
+  }
 
 }
--- app/Providers/BladeServiceProvider.php
+++ app/Providers/BladeServiceProvider.php
@@ -5,21 +5,19 @@ use Illuminate\Support\ServiceProvider;
 
 class BladeServiceProvider extends ServiceProvider {
 
+  public function boot()
+  {
+    Blade::directive('gravatar', function() {
+      return '<?="http://www.gravatar.com/avatar/" . md5( strtolower( trim( $user->email ) ) ) . "?d=mm";?>';
+    });
+
+    Blade::directive('csrf', function() {
+      return '<?="<input type=\"hidden\" name=\"_token\" value=\"".csrf_token()."\">";?>';
+    });
+  }
+
   public function register()
   {
-    // Blade Templating Extensions
-    Blade::extend(function($view, $compiler)
-    {
-      $pattern = $compiler->createPlainMatcher('gravatar');
-      return preg_replace($pattern, '<?="http://www.gravatar.com/avatar/" . md5( strtolower( trim( $user->email ) ) ) . "?d=mm";?>', $view);
-    });
-
-    Blade::extend(function($view, $compiler)
-    {
-      $pattern = $compiler->createPlainMatcher('csrf');
-      return preg_replace($pattern, '<?="<input type=\"hidden\" name=\"_token\" value=\"".csrf_token()."\">";?>', $view);
-    });
-
 
   }
 
