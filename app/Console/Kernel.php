7cd338915a3025ce997f6ef061b7dba5829be112
Paul Williams <paul@skenmy.com>
2015-08-31 16:23:52 +0100

    Leaderboards to cron

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
+use Illuminate\Support\Facades\Cache;
+use vAMSYS\Pirep;
 
 class Kernel extends ConsoleKernel {
 
@@ -15,7 +17,8 @@ class Kernel extends ConsoleKernel {
         'vAMSYS\Console\Commands\ImportCountries',
         'vAMSYS\Console\Commands\ImportRegions',
         'vAMSYS\Console\Commands\ImportAirports',
-        'vAMSYS\Console\Commands\ParseTextDataCommand'
+        'vAMSYS\Console\Commands\ParseTextDataCommand',
+        'vAMSYS\Console\Commands\vramsimport'
     ];
 
     /**
@@ -26,7 +29,45 @@ class Kernel extends ConsoleKernel {
      */
     protected function schedule(Schedule $schedule)
     {
+        $schedule->call(function () {
+            $pirepsByUser = null;
+            Pirep::where('pirep_data', '!=', '{"jumpseat":true}')->chunk(100, function ($pirepsChunk) use (&$pirepsByUser) { // TODO Improve this detection...
+                $pirepsByUser = $pirepsChunk->groupBy(function ($item, $key) {
+                    return $item->booking->pilot->id;
+                });
+            });
 
+            // Most Points
+            $pointsPirepsByUser = $pirepsByUser->sortByDesc(function (&$item, $key){
+                $totalPoints = 0;
+                foreach ($item as $pirep){
+                    $totalPoints += $pirep->points;
+                }
+                $item->totalPoints = $totalPoints;
+                return $totalPoints;
+            });
+
+            // Most Hours
+            $timePirepsByUser = $pirepsByUser->sortByDesc(function (&$item, $key){
+                $totalSeconds = 0;
+                foreach ($item as $pirep){
+                    $totalSeconds += $pirep->landing_time->getTimestamp() - $pirep->departure_time->getTimestamp();
+                }
+                $item->totalInterval = Carbon::now()->addSeconds($totalSeconds);
+                return $totalSeconds;
+            });
+
+            // Most PIREPs
+            $countPirepsByUser = $pirepsByUser->sortByDesc(function ($item, $key){
+                return count($item);
+            });
+
+            Cache::forever('RYR:Leaderboards:Global', [
+              'pirepCounts' => $countPirepsByUser->slice(0, 30),
+              'pirepHours' => $timePirepsByUser->slice(0, 30),
+              'pirepPoints' => $pointsPirepsByUser->slice(0, 30)
+            ]); // TODO: Make this run for each airline
+        })->hourly();
     }
 
 }
--- app/Http/Controllers/LeaderboardsController.php
+++ app/Http/Controllers/LeaderboardsController.php
@@ -23,47 +23,7 @@ class LeaderboardsController extends Controller {
 	public function getIndex()
     {
         // Collect statistics if required
-        $data = Cache::remember(PilotRepository::getCurrentPilot()->airline->prefix.':Leaderboards:Global', 15, function(){
-
-            $pirepsByUser = null;
-            Pirep::where('pirep_data', '!=', '{"jumpseat":true}')->chunk(100, function ($pirepsChunk) use (&$pirepsByUser) { // TODO Improve this detection...
-                $pirepsByUser = $pirepsChunk->groupBy(function ($item, $key) {
-                    return $item->booking->pilot->id;
-                });
-            });
-
-            // Most Points
-            $pointsPirepsByUser = $pirepsByUser->sortByDesc(function (&$item, $key){
-                $totalPoints = 0;
-                foreach ($item as $pirep){
-                    $totalPoints += $pirep->points;
-                }
-                $item->totalPoints = $totalPoints;
-                return $totalPoints;
-            });
-
-            // Most Hours
-            $timePirepsByUser = $pirepsByUser->sortByDesc(function (&$item, $key){
-                $totalSeconds = 0;
-                foreach ($item as $pirep){
-                    $totalSeconds += $pirep->landing_time->getTimestamp() - $pirep->departure_time->getTimestamp();
-                }
-                $item->totalInterval = Carbon::now()->addSeconds($totalSeconds);
-                return $totalSeconds;
-            });
-
-            // Most PIREPs
-            $countPirepsByUser = $pirepsByUser->sortByDesc(function ($item, $key){
-                return count($item);
-            });
-
-            return [
-                'pirepCounts' => $countPirepsByUser->slice(0, 30),
-                'pirepHours' => $timePirepsByUser->slice(0, 30),
-                'pirepPoints' => $pointsPirepsByUser->slice(0, 30)
-            ];
-
-        });
+        $data = Cache::get(PilotRepository::getCurrentPilot()->airline->prefix.':Leaderboards:Global');
 
 		return view('leaderboards.global', ['data' => (object)$data]);
 	}
